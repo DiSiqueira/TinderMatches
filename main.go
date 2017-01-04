9e07babc9ca0e271f468f49b165938483d52d909
DiSiqueira <dieg0@live.com>
2017-01-04 15:08:00 -0300

    Creating first files

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/disiqueira/tindergo"
+)
+
+func main() {
+	token := flag.String("token", "", "Your Facebook Token.")
+
+	flag.Parse()
+
+	if *token == "" {
+		fmt.Println("You must provide a valid Facebook Token.")
+		os.Exit(2)
+	}
+
+	t := tindergo.New()
+
+	err := t.Authenticate(*token)
+	checkError(err)
+
+	profile, err := t.Profile()
+	checkError(err)
+
+	fmt.Println("Your Profile:")
+	fmt.Println("Name: " + profile.Name)
+	fmt.Println("")
+
+	var allRecs []tindergo.RecsCoreUser
+
+	for j := 0; j <= 3; j++ {
+		recs, err := t.RecsCore()
+		checkError(err)
+		allRecs = append(allRecs, recs)
+	}
+
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
